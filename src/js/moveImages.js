0f23905b71ff4a81a8f4707ae342cd3e96ae3d5c
robertlocskai <robertlocskai@gmail.com>
2023-09-12 13:23:33 +0200

    feat: image hide on mobile view + animation + responsible. BREAKING: new screen sizes

---
+++ src/js/moveImages.js
@@ -0,0 +1,18 @@
+$(window).scroll(function() {
+  var scrollTop = $(window).scrollTop();
+  console.log("scrollTop>>>" + scrollTop);
+    if (scrollTop == 0) {
+
+        $("#headerImages").css({"margin-top": -($(window).scrollTop()) + "px"});    
+  
+    } 
+  
+  else {
+
+    if(scrollTop < 150) {
+
+        $("#headerImages").css({"margin-top": -($(window).scrollTop())/5 + "px"});   
+
+    }
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
         'headerBg': "url('/src/images/headerBg.jpg')"
       },
     },
+    screens: {
+      'sm': {'min': '640px', 'max': '767px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '768px', 'max': '1135px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1135px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+    },
   },
   plugins: [],
 }
