258a966cdc6ccc98050f07e38eddb9cfb0080b36
robertlocskai <robertlocskai@gmail.com>
2023-09-13 15:43:00 +0200

    feat: technologia section, and pictures added, kind of responsive. + feat: tailwind rebuilding start bat file

--- src/js/moveImages.js
+++ src/js/moveImages.js
@@ -14,5 +14,6 @@ $(window).scroll(function() {
         $("#headerImages").css({"margin-top": -($(window).scrollTop())/5 + "px"});   
 
     }
+    
   }
 });
---
+++ start.bat
@@ -0,0 +1,2 @@
+start cmd /k code .
+npx tailwindcss -i ./src/css/input.css -o ./src/css/output.css --watch
